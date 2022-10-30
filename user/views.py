7a6c5d24e2f3e876a754ae06b2e129846d72fb8b
juniorrestituyo <juniorrestituyo@outlook.com>
2022-10-30 00:09:32 -0400

    working with sign up

--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.urls import is_valid_path
 
 from .models import Profile
 from .forms import CustomUserCreationForm
@@ -39,6 +40,7 @@ def logoutUser(request):
     return redirect('login')
 
 
+@login_required(login_url='login')
 def logged(request):
     return render(request, 'logged.html')
 
@@ -46,7 +48,21 @@ def logged(request):
 def signup(request):
     page = 'signup'
     form = CustomUserCreationForm()
-    
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account was created")
+
+            login(request, user)
+            return redirect('login')
+        
+        else:
+            messages.error(request, "An error has occurred during registration")
 
     context = {'page': page, 'form': form}
     return render(request, 'index.html', context)
