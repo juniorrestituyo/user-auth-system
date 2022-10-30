882ba248b821ddff312cd026de7623c479d874c9
juniorrestituyo <juniorrestituyo@outlook.com>
2022-10-29 22:15:53 -0400

    adding more changes in the login

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('loged/', views.loged, name='loged'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup')
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 def login(request):
@@ -9,6 +13,13 @@ def login(request):
 
 def logout(request):
     return render(request, 'index.html')
+
+
+def loged(request):
+    page = 'loged'
+
+    context = {'page': page}
+    return render(request, 'index.html', context)
 
 
 def signup(request):
