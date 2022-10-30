cc58c9e55396961fd51963e8e7bb4ef1c12d22b2
juniorrestituyo <juniorrestituyo@outlook.com>
2022-10-30 00:25:31 -0400

    working with the sign up

--- user/signals.py
+++ user/signals.py
@@ -1,4 +1,3 @@
-import profile
 from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 from .models import Profile
@@ -15,4 +14,24 @@ def createProfile(sender, instance, created, **kwargs):
         )
 
 
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
+def deleteUser(sender, instance, **kwargs):
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
+
+
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
+post_delete.connect(deleteUser, sender=Profile)
