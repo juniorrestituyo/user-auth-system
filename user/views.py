f2319c826e4ee29ace608a2acf5d9e0b98495c56
juniorrestituyo <juniorrestituyo@outlook.com>
2022-10-29 22:07:20 -0400

    working with login and sign up

--- user/views.py
+++ user/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 
 def login(request):
+    page = 'login'
+
     return render(request, 'index.html')
+
+
+def logout(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    page = 'signup'
+
+    context = {'page': page}
+    return render(request, 'index.html', context)
